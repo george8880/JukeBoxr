d8260fab2a95d3b62ee5f648b87d053b0f45d03d
Yuqi <zhuyu@wharton.upenn.edu>
2013-09-07 01:58:52 -0400

    testing branch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JukeBoxr::Application.config.secret_token = '82c283c1cd7b81cbf4ef20dc2939f3ccb766e81df4431056785fb5693f59a601c7eab36e70efabbfc9dece17b24e61a4d3a61ee8bf85c57fa74b07d5e3806421'
